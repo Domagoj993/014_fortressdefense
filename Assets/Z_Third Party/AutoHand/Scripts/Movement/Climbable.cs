9cf8b903eaf49a7c09de9085cd507ad398471d4d
ivzoric <ivzoric88@gmail.com>
2022-02-21 16:03:02 +0100

    Enemy ships + auto hand removed

--- Assets/Z_Third Party/AutoHand/Scripts/Movement/Climbable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Autohand {
-    [RequireComponent(typeof(Grabbable))]
-    public class Climbable : MonoBehaviour{
-        public Vector3 axis = Vector3.one;
-    }
-}
